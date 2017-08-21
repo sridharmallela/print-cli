02543e424ecaee8aaf1b8b7aa79ccbce57296d8a
Mallela, Sridhar <sridhar.mallela@fmr.com>
2017-08-21 00:28:50 -0500

    chore(print-cli): updated version

--- test/print-cli.spec.js
+++ test/print-cli.spec.js
@@ -2,6 +2,7 @@
 
 const spawn = require("child_process").spawn,
     expect = require("chai").expect,
+    version = require('./../package.json').version,
     EXEC_PATH = require('path').resolve(`${__dirname}/../src/print-cli.js`),
     TEST_DATA_RES = '______ ______ _____ ______   ____   ___   ______ ___ \n      /_  __// ____// ___//_  __/  / __ \\ /   | /_  __//   |\n       / /  / __/   \\__ \\  / /    / / / // /| |  / /  / /| |\n      / /  / /___  ___/ / / /    / /_/ // ___ | / /  / ___ |\n     /_/  /_____/ /____/ /_/    /_____//_/  |_|/_/  /_/  |_|\n                                                            \n',
     TEST_HELP_RES = '\n  Usage: print-cli [options] [text]\n\n\n  Options:\n\n        --version          output the version number\n        --banner           print banner in ASCII style\n    -f  --font [value]     font used to print text [standard|doom|slant|bell|chunky]\n    -c  --color [value]    color of the printed text [blue|black|cyan|green|grey|magenta|red|white|yellow]\n        --bgColor [value]  background color of the printed text [bgBlack|bgBlue|bgCyan|bgGreen|bgMagenta|bgRed|bgWhite|bgYellow]\n    -h, --help             output usage information\n\n  Examples:\n\n    $ print-cli --color red "print banner"\n    $ print-cli --help\n\n';
@@ -59,8 +60,8 @@ describe('src/print-cli --- ', () => {
                 expect(err).to.be.eql(null);
                 expect(stdout).not.to.be.empty;
                 expect(stdout).not.to.be.undefined;
-                expect(stdout).to.contain('1.0.1');
-                expect(stdout).to.eql('1.0.1\n');
+                expect(stdout).to.contain(version);
+                expect(stdout).to.eql(version + '\n');
                 done();
             });
         });
